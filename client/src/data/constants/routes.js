bdd1dd40f782d6a8c4a8329b841eabf95b62ebc6
Derion Williams <derionwllms@gmail.com>
2023-03-24 19:26:39 -0500

    added semicolon line endings

--- client/src/data/constants/routes.js
+++ client/src/data/constants/routes.js
@@ -5,8 +5,8 @@ export const ACCOUNT = "/account";
 export const COLLECTION = "/collection";
 export const WISHLIST = "/wishlist";
 export const LANDING = "/";
-export const RECENT_FAVORITES = "/favorites"
-export const RECENT_RELEASES = "/releases"
-export const UPCOMING_RELEASES = "/upcoming"
+export const RECENT_FAVORITES = "/favorites";
+export const RECENT_RELEASES = "/releases";
+export const UPCOMING_RELEASES = "/upcoming";
 
 export const SERVER_URL = "http://127.0.0.1:4444"
