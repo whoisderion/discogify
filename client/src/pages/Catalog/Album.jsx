a769ff16844645579530923ee29d8623234d1cb2
Derion Williams <derionwllms@gmail.com>
2023-03-27 00:20:10 -0500

    Added styling

--- client/src/pages/Catalog/Album.jsx
+++ client/src/pages/Catalog/Album.jsx
@@ -35,19 +35,19 @@ const Album = () => {
                 <div className='marketListings'>
                     <h3>Vinyl on the market</h3>
                     {/* displayAlbums(searchResults) */}
-                    <div className='results'>
+                    <div className='results inline-grid grid-cols-4'>
                         {
                             searchResults.map((result) => (
-                                <div key={result.id}>
-                                    <p>Units Available: {result.numForSale}</p>
-                                    <p>Lowest Price: {result.lowestPrice}</p>
-                                    <p>Vinyl Type:
+                                <div className='result border-2 border-slate-200 m-4' key={result.id}>
+                                    <p className='available-units'>Units Available: {result.numForSale}</p>
+                                    <p className='lowest-price'>Lowest Price: ${result.lowestPrice}</p>
+                                    <p className='vinyl-types'>Vinyl Type:
                                         {result.descriptions.descriptions}
                                     </p>
-                                    <p>Additional Info: {result.descriptions.text}</p>
-                                    <p>Discogs ID: {result.id}</p>
-                                    <p>Release Date: {result.dateReleased}</p>
-                                    <p>Discogs URL: {result.marketplaceLink}</p>
+                                    <p className='additional-info'>Additional Info: {result.descriptions.text}</p>
+                                    <p className='discogs-id'>Discogs ID: {result.id}</p>
+                                    <p className='release-date'>Release Date: {result.dateReleased}</p>
+                                    {/* <p className='discogs-url'>Discogs URL: {result.marketplaceLink}</p> */}
                                 </div>
                             ))
                         }
