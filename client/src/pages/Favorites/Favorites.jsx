5f3eb348a9a019b68640006c63cecd37f4504d77
Derion Williams <derionwllms@gmail.com>
2023-06-20 10:49:28 -0500

    Updated to reflect change in the props of <Row/>

--- client/src/pages/Favorites/Favorites.jsx
+++ client/src/pages/Favorites/Favorites.jsx
@@ -9,7 +9,7 @@ const Favorites = () => {
     const numRows = Math.ceil(50 / rowLength)
     const favoriteTracks = JSON.parse(localStorage.getItem('favoriteSongs'))
     const favoriteArtists = JSON.parse(localStorage.getItem('favoriteArtists'))
-    //console.log(favoriteTracks.length / rowLength)
+
     const types = {
         'favorite_artists': {
             name: 'artist',
@@ -23,19 +23,7 @@ const Favorites = () => {
 
     const handleView = () => {
         view == "favorite_tracks" ? setView("favorite_artists") : setView("favorite_tracks")
-        //console.log(view)
     }
-
-    function showRows() {
-        var rows = []
-        for (let index = 0; index < numRows; index++) {
-            rows.push(<Row type={types[view]} index={index} />)
-        }
-        return rows
-    }
-
-    const arr = showRows()
-    //console.log(arr)
 
     return (
         <div>
@@ -44,19 +32,7 @@ const Favorites = () => {
                 <button onClick={handleView}>Songs</button>
                 <button onClick={handleView}>Artists</button>
             </div>
-            {/* {view == 'favorite_tracks' ? showTracks() : showArtists()} */}
-            {/* {showItems(types[view], 0)} */}
-            {/* {showItems(types[view], 6)} */}
-            {/* {showRows()} */}
-            <Row type={types[view]} index={0} rowLength={rowLength} />
-            <Row type={types[view]} index={6} rowLength={rowLength} />
-            <Row type={types[view]} index={12} rowLength={rowLength} />
-            <Row type={types[view]} index={18} rowLength={rowLength} />
-            <Row type={types[view]} index={24} rowLength={rowLength} />
-            <Row type={types[view]} index={30} rowLength={rowLength} />
-            <Row type={types[view]} index={36} rowLength={rowLength} />
-            <Row type={types[view]} index={42} rowLength={rowLength} />
-            <Row type={types[view]} index={48} rowLength={rowLength} />
+            <Row type={types[view]} rowLength={rowLength} />
         </div>
     )
 }
