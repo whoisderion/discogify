6ad9d3be1dc6bd4be1e1cf5b3fcce05004aa2cec
Derion Williams <derionwllms@gmail.com>
2023-06-20 10:47:44 -0500

    Removed array creation from return statements

--- client/src/components/ui/Row/Row.jsx
+++ client/src/components/ui/Row/Row.jsx
@@ -1,84 +1,112 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
-const Row = ({ type, index, rowLength, store }) => {
+const Row = ({ type, rowLength, store, maxRows }) => {
     if (type.name == 'track') {
+        const createComponents = () => {
+            let itemsArr = []
+            type.store.data.forEach((item) => {
+                itemsArr.push(
+                    <Link key={item.id} id={item.id} to={`/catalog/${encodeURIComponent(String(item.artist).replace(/\s+/g, '-'))}/${encodeURIComponent(String(item.album).replace(/\s+/g, '-'))}?track=${item.name}`}>
+                        <div className={`${item.name}`}>
+                            <div className={`${type.name}-image`}>
+                                <img src={item.image} className='aspect-square object-cover' />
+                            </div>
+                            <p>{item.name}</p>
+                            {item.artist && <p>{item.artist}</p>}
+                        </div>
+                    </Link>
+                )
+            })
+            if (maxRows) {
+                return itemsArr.slice(0, (rowLength * maxRows))
+            }
+            return itemsArr
+        }
         return (
             < div className={`${type.name}-layout inline-grid grid-cols-6 mb-4`}>
-                {
-                    type.store.data.slice(index, (index + rowLength)).map((item, itemIndex) => (
-                        <Link key={item.id} id={item.id} to={`/catalog/${encodeURIComponent(String(item.artist).replace(/\s+/g, '-'))}/${encodeURIComponent(String(item.album).replace(/\s+/g, '-'))}?track=${item.name}`}>
-                            <div className={`${item.name}`}>
-                                <div className={`${type.name}-image`}>
-                                    <img src={item.image} className='aspect-square object-cover' />
-                                </div>
-                                {/* <p>{index + itemIndex}</p> */}
-                                <p>{item.name}</p>
-                                {item.artist && <p>{item.artist}</p>}
-                            </div>
-                        </Link>
-                    ))
-                }
+                {createComponents()}
             </div >
         )
     } else if (type.name == 'artist') {
+        const createComponents = () => {
+            let itemsArr = []
+            type.store.data.forEach((item) => {
+                itemsArr.push(
+                    <Link key={item.id} id={item.id} to={`/catalog/${encodeURIComponent(String(item.name).replace(/\s+/g, '-'))}`}>
+                        <div className={`${item.name}`}>
+                            <div className={`${type.name}-image`}>
+                                <img src={item.image} className='aspect-square object-cover' />
+                            </div>
+                            <p>{item.name}</p>
+                            {item.artist && <p>{item.artist}</p>}
+                        </div>
+                    </Link>
+                )
+            })
+            if (maxRows) {
+                return itemsArr.slice(0, (rowLength * maxRows))
+            }
+            return itemsArr
+        }
         return (
             < div className={`${type.name}-layout inline-grid grid-cols-6 mb-4`}>
                 {
-                    type.store.data.slice(index, (index + rowLength)).map((item) => (
-                        <Link key={item.id} id={item.id} to={`/catalog/${encodeURIComponent(String(item.name).replace(/\s+/g, '-'))}`}>
-                            <div className={`${item.name}`}>
-                                <div className={`${type.name}-image`}>
-                                    <img src={item.image} className='aspect-square object-cover' />
-                                </div>
-                                <p>{item.name}</p>
-                                {item.artist && <p>{item.artist}</p>}
-                            </div>
-                        </Link>
-                    ))
+                    createComponents()
                 }
             </div >
         )
     } else if (type.name == 'album') {
+        const { artist } = useParams()
+        const createComponents = () => {
+            let itemsArr = []
+            store.forEach((item) => {
+                itemsArr.push(
+                    <div key={item.albumID}>
+                        <Link to={`/catalog/${artist}/${encodeURIComponent(String(item.albumName).replace(/\s+/g, '-'))}`}>
+                            <img src={item.imageMD.url}></img>
+                            <p>
+                                {item.albumName}
+                            </p>
+                        </Link>
+                        <p>
+                            {item.releaseDate}
+                        </p>
+                    </div>
+                )
+            })
+            return itemsArr
+        }
         return (
-            <div className={`${type.name}-layout inline-grid grid-cols-${rowLength} mb-4`}>
+            <div className={`${type.name}-layout inline-grid grid-cols-5 mb-4`}>
                 {
-                    store.slice(index, (index + rowLength)).map((item) => {
-                        // console.log(item.albumName)
-                        return (
-                            <div key={item.albumID}>
-                                <img src={item.imageMD.url}></img>
-                                <p>
-                                    {item.albumName}
-                                </p>
-                                <p>
-                                    {item.releaseDate}
-                                </p>
-                            </div>
-                        )
-                    })
+                    createComponents()
                 }
             </div>
         )
     } else if (type.name == 'single') {
-        return (
-            <div className={`${type.name}-layout inline-grid grid-cols-${rowLength} mb-4`}>
-                {
-                    store.slice(index, (index + rowLength)).map((item) => {
-                        // console.log(item.albumName)
-                        return (
-                            <div key={item.albumID}>
+        const createComponents = () => {
+            let itemsArr = []
+            store.forEach((item) => {
+                itemsArr.push(
+                    <div key={item.albumID}>
+                        <img src={item.imageMD.url}></img>
+                        <p>
+                            {item.albumName}
+                        </p>
+                        <p>
+                            {item.releaseDate}
+                        </p>
+                    </div>
+                )
+            })
+            return itemsArr
+        }
 
-                                <img src={item.imageMD.url}></img>
-                                <p>
-                                    {item.albumName}
-                                </p>
-                                <p>
-                                    {item.releaseDate}
-                                </p>
-                            </div>
-                        )
-                    })
+        return (
+            <div className={`${type.name}-layout inline-grid grid-cols-5 mb-4`}>
+                {
+                    createComponents()
                 }
             </div>
         )
