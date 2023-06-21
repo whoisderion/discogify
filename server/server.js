f7d6d64520e1d34bda1c1695037dfda90451c7e8
Derion Williams <derionwllms@gmail.com>
2023-06-21 00:18:46 -0500

    CORS test

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const prisma = new PrismaClient()
 const SCOPE = ['playlist-read-private user-top-read user-library-read user-read-private user-read-email']
 const SPOTIFY_API_URL = "https://api.spotify.com/v1"
 const corsOptions = {
-    origin: ['http://127.0.0.1:5173', 'https://accounts.spotify.com', 'https://api.discogs.com', 'https://whale-app-ebaic.ondigitalocean.app'],
+    origin: "*",
     methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
