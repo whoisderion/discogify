22499e3fdd88848bef6e0a64d5315ba0caf7bfa9
Derion Williams <derionwllms@gmail.com>
2023-06-27 19:13:04 -0500

    Redirct test

--- server/server.js
+++ server/server.js
@@ -134,7 +134,7 @@ app.get('/spotify/callback', (req, res) => {
             res.cookie('accessToken', accessToken, { httpOnly: true })
             res.cookie('refreshToken', refreshToken, { httpOnly: true, })
 
-            res.redirect('https://discogify.com/discogify-server/close')
+            res.send('https://discogify.com/discogify-server/close')
         })
         .catch((error) => {
             res.send(error);
