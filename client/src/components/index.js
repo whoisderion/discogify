b6cbcab9d3d79625a65ad8898de81a56aba51877
Derion Williams <derionwllms@gmail.com>
2024-01-10 09:51:20 -0600

    Created module file for directory

---
+++ client/src/components/index.js
@@ -0,0 +1,9 @@
+import ProtectedRoute from "./ProtectedRoute";
+import PublicRoute from "./PublicRoute";
+import Footer from "./Footer";
+import FormInput from "./form/FormInput";
+import NavigationBar from "./NavigationBar";
+import Carousel from "./ui/Carousel";
+import Row from "./ui/Row";
+
+export {ProtectedRoute, PublicRoute, Footer, FormInput, NavigationBar, Carousel, Row}
